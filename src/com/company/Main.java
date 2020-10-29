c1f4d74d00b140eafd4d211d2085dfef2bcb5dba
aktan-alt <aktan.kolbaev98@gmail.com>
2020-10-29 20:54:31 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(myAge(33,21));
+        System.out.println(myAge(12,43));
+        System.out.println(myAge(22,-11));
+        System.out.println(myAge(23,-21));
+        System.out.println(myAge(19,21));
+
+    }
+
+
+    public static String myAge(int age, int temp) {
+        if (age > 20 && age < 45 && temp > -20 && temp < 30) {
+            return "Можно идти гулять";
+        } else if (age < 20 && temp > 0 && temp < 28) {
+            return "Можно идти гулять";
+        } else if (age > 45 && temp > -10 && temp < 25) {
+            return "Можно идти гулять";
+        } else {
+            return "Оставайтесь дома";
+        }
+
+    }
+}
